57342ab6d61959448cc621ac39f54118034c1ff0
daisy-bait <kadanarpa@gmail.com>
2026-06-13 18:40:28 -0500

    [KAD-25] fix: login and session models

--- users_module/src/main/java/co/edu/usco/convocations/server/users/domain/model/auth/LoginResponse.java
+++ users_module/src/main/java/co/edu/usco/convocations/server/users/domain/model/auth/LoginResponse.java
@@ -2,12 +2,19 @@ package co.edu.usco.convocations.server.users.domain.model.auth;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder
 @Getter
 @Setter
+@NoArgsConstructor
 public class LoginResponse extends SessionResponse {
+
+    public LoginResponse(String userId, String accessToken, String refreshToken) {
+        super(accessToken);
+        this.refreshToken = refreshToken;
+        this.userId = userId;
+    }
 
     private String refreshToken;
     private String userId;
--- users_module/src/main/java/co/edu/usco/convocations/server/users/domain/model/auth/SessionResponse.java
+++ users_module/src/main/java/co/edu/usco/convocations/server/users/domain/model/auth/SessionResponse.java
@@ -3,9 +3,12 @@ package co.edu.usco.convocations.server.users.domain.model.auth;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class SessionResponse {
