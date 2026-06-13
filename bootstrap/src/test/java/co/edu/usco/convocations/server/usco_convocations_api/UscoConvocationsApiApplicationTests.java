913332e3db6c77cd29561f162e76668a814fdb6c
daisy-bait <kadanarpa@gmail.com>
2026-06-13 00:42:37 -0500

    [KAD-25] feat: add all the properties for db and external services connections

--- bootstrap/src/test/java/co/edu/usco/convocations/server/usco_convocations_api/UscoConvocationsApiApplicationTests.java
+++ bootstrap/src/test/java/co/edu/usco/convocations/server/usco_convocations_api/UscoConvocationsApiApplicationTests.java
@@ -1,13 +1,27 @@
 package co.edu.usco.convocations.server.usco_convocations_api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.oauth2.server.resource.autoconfigure.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "APP_PORT=0"
+})
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        OAuth2ResourceServerAutoConfiguration.class
+})
 class UscoConvocationsApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
